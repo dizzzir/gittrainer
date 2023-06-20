538dc8e39667df8766bdd34ea8e0dd024c6f7d91
Azamat Iskindirov <dizzzir@gmail.com>
2023-06-21 00:38:11 +0600

    Switch case

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-let a = 5;
-let b = 5;
-let c = a * b;
-console.log(c);
+const role = 'manager';
+
+if (role === 'manager') {
+    console.log('Добро пожаловать, Менеджер!');
+} else if (role === 'admin') {
+    console.log('Добро пожаловать, Админ!');
+} else if (role === 'seo') {
+    console.log('Добро пожаловать, СЕО!');
+} else {
+    console.log('Ты кто такой?')
+}
+
+switch (role) {
+    case 'manager':
+    console.log('Добро пожаловать, Менеджер!');  
+    break;
+    case 'admin':
+    console.log('Добро пожаловать, Админ!'); 
+    break;   
+    default:
+        console.log('Ты кто такой?');    
+}
